8ef2980d54cbffc7dfa274152bdf75e14203c4b2
sya-ri <git@s7a.dev>
2025-05-21 15:33:46 +0900

    feat: Implement bin

---
+++ bin/start.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { serve } from "../src/index.js";
+
+const port = Number.parseInt(process.argv[2] ?? "8080", 10);
+if (Number.isNaN(port)) {
+	console.error("Invalid port number");
+	process.exit(1);
+}
+
+console.log(`Starting echomock server on port ${port}...`);
+serve(port);
